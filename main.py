51d7afc51b785c12de9672aae905f614693915f0
Julius Ramshage <jura5584@student.su.se>
2024-10-10 16:32:21 +0200

    set up selenium

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
